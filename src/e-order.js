6c3e6164595faacffd9f4bfb18cce385df44e75a
Nikos Zapantis <86017435+NikosZapantis@users.noreply.github.com>
2023-06-22 19:34:41 +0300

    New function for quantity index search {Add}

--- src/e-order.js
+++ src/e-order.js
@@ -19,14 +19,14 @@ const products = [
 // Define the cartItems array
 let cartItems = [];
 
-//Function that creates the categories and displaying them
+// Function that creates the categories and displays them
 function displayCategories() {
     const categoryList = document.getElementById('category-list');
 
-    //Clearing existing content
+    // Clearing existing content
     categoryList.innerHTML = '';
 
-    //Loop through categories and creating buttons
+    // Loop through categories and create buttons
     categories.forEach(category => {
         const button = document.createElement('button');
         button.innerText = category.name;
@@ -35,78 +35,82 @@ function displayCategories() {
     });
 }
 
-//Function to retrieve the quantity of specific product
+// Function to retrieve the quantity of a specific product from the cart
 function getProductQuantity(productId) {
-    const selectedProduct = products.find(product => product.id === productId);
+    const selectedProduct = cartItems.find(item => item.id === productId);
     return selectedProduct ? selectedProduct.quantity : 0;
 }
 
-//Function to filter and display products by category
+// Function to filter and display products by category
 function filterProducts(categoryId) {
     const productList = document.getElementById('product-list');
 
-    //Clearing existing content
+    // Clearing existing content
     productList.innerHTML = '';
 
-    //Filtering products by category
+    // Filtering products by category
     const filteredProducts = products.filter(product => product.categoryId === categoryId);
 
-    //Loop through filtered products and creating HTML elements
+    // Loop through filtered products and create HTML elements
     filteredProducts.forEach(product => {
         const item = document.createElement('div');
         item.innerHTML = `
-            <h3>${product.name} (Price: $${product.price}) / 
-                <button class="quantity-btn" onclick="addToCart(${product.id})">+</button> 
-                ${getProductQuantity(product.id)} 
-                <button class="quantity-btn" onclick="removeFromCart(${product.id})">-</button>
-            </h3>
+        <h3>${product.name} (Price: $${product.price}) / 
+            <button class="quantity-btn" onclick="addToCart(${product.id})">+</button> 
+            ${getProductQuantity(product.id)} 
+            <button class="quantity-btn" onclick="removeFromCart(${product.id})">-</button>
+        </h3>
         `;
         productList.appendChild(item);
     });
 }
 
-//Function to add a product to the cart
+// Function to add a product to the cart
 function addToCart(productId) {
     const selectedProduct = products.find(product => product.id === productId);
 
-    //Checking if product is already in the cart
-    const isInCart = cartItems.some(item => item.id == selectedProduct.id);
+    // Checking if the product is already in the cart
+    const isInCart = cartItems.some(item => item.id === selectedProduct.id);
 
-    if(!isInCart) { //if the product doesnt exist in the cart I push it and increase the quantity index
+    if (!isInCart) {
+        // Increase the quantity by 1
         selectedProduct.quantity++;
 
+        // Push the selected product to the cart
         cartItems.push(selectedProduct);
-    }else { //If exist then I just increase the quantity index
+    } else {
+        // If the product already exists in the cart, just increase the quantity
         selectedProduct.quantity++;
     }
 }
 
-//Function to remove a product of the cart
+// Function to remove a product from the cart
 function removeFromCart(productId) {
-    const selectedProduct = products.find(product => product.id == productId);
+    const selectedProduct = products.find(product => product.id === productId);
 
-    //Finding the index of the selected product in the cart
-    const cartIndex = cartItems.findIndex(item => item.id == selectedProduct.id);
+    // Finding the index of the selected product in the cart
+    const cartIndex = cartItems.findIndex(item => item.id === selectedProduct.id);
 
-    if(cartIndex !== -1) {
-        //Decreasing the quantity index by 1
+    if (cartIndex !== -1) {
+        // Decrease the quantity by 1
         selectedProduct.quantity--;
 
-        if(selectedProduct.quantity === 0) {
-            cartItems.splice(cartIndex, 1);
+        if (selectedProduct.quantity === 0) {
+        // Remove the product from the cart if quantity reaches 0
+        cartItems.splice(cartIndex, 1);
         }
     }
 }
 
-//Function to view the cart page
+// Function to view the cart page
 function viewCart() {
     window.location.href = 'my-cart.html';
 }
 
-//Function to redirect to main page if the user is on my-cart page
+// Function to redirect to the main page if the user is on the my-cart page
 function redirectToMainPage() {
     window.location.href = 'index.html';
 }
 
-//Displaying the categories
+// Displaying the categories
 displayCategories();
