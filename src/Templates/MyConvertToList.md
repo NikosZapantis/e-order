8a8154f85079324dddadd44715af253dec98b0b5
Nikos Zapantis <86017435+NikosZapantis@users.noreply.github.com>
2023-07-13 20:43:29 +0300

    Function for Product List download {Add}

---
+++ src/Templates/MyConvertToList.md
@@ -0,0 +1,30 @@
+// Converting All items into a list in excel
+function convertCartToExcelList() {
+  // Creating an HTML table with borders
+  let htmlContent = '<table border="1">';
+
+  // Add table headers
+  htmlContent += '<tr><th>Προϊόν</th><th>Κατηγορία</th></tr>';
+
+  // Iterate over each product
+  AllProducts.forEach(product => {
+    const category = categories.find(cat => cat.id === product.categoryId);
+    if (category) {
+      // Add a table row with the product name and category name
+      htmlContent += `<tr><td>${product.name}</td><th>${category.name}</th></tr>`;
+    }
+  });
+
+  htmlContent += '</table>';
+
+  // Create a Blob with the HTML content
+  const blob = new Blob([htmlContent], { type: 'application/vnd.ms-excel' });
+
+  // Create the download link
+  const url = URL.createObjectURL(blob);
+  const downloadLink = createDownloadLink(url, 'ProductList.xls');
+  downloadLink.style.display = 'none';
+  document.body.appendChild(downloadLink);
+
+  downloadLink.click();
+}
--- src/js_extensions/active-products.js
+++ src/js_extensions/active-products.js
@@ -47,6 +47,45 @@ function displayAllProducts() {
     });
 }
 
+// Converting All items into a list in excel
+function convertCartToExcelList() {
+  // Creating an HTML table with borders
+  let htmlContent = '<table border="1">';
+
+  // Add table headers
+  htmlContent += '<tr><th>Προϊόν</th><th>Κατηγορία</th></tr>';
+
+  // Iterate over each product
+  AllProducts.forEach(product => {
+    const category = categories.find(cat => cat.id === product.categoryId);
+    if (category) {
+      // Add a table row with the product name and category name
+      htmlContent += `<tr><td>${product.name}</td><th>${category.name}</th></tr>`;
+    }
+  });
+
+  htmlContent += '</table>';
+
+  // Create a Blob with the HTML content
+  const blob = new Blob([htmlContent], { type: 'application/vnd.ms-excel' });
+
+  // Create the download link
+  const url = URL.createObjectURL(blob);
+  const downloadLink = createDownloadLink(url, 'ProductList.xls');
+  downloadLink.style.display = 'none';
+  document.body.appendChild(downloadLink);
+
+  downloadLink.click();
+}
+
+//Creating download link
+function createDownloadLink(content, filename) {
+  const downloadLink = document.createElement('a');
+  downloadLink.href = content;
+  downloadLink.download = filename;
+  return downloadLink;
+}
+
 // Function to toggle the active state of a product
 function toggleProductActiveState(productId, isActive) {
     const currentlyAllProducts = JSON.parse(localStorage.getItem('AllProducts'));
